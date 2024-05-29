72615620e48d2c7c893b43539c5e75ba90e170e2
Mason <theforbiddenai@gmail.com>
2024-05-28 21:00:56 -0400

    Changed WebScraper to use InputStream instead of String

--- src/main/java/me/theforbiddenai/gamefinder/webscraper/GOGWebScrape.java
+++ src/main/java/me/theforbiddenai/gamefinder/webscraper/GOGWebScrape.java
@@ -3,105 +3,130 @@ package me.theforbiddenai.gamefinder.webscraper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.NonNull;
 import me.theforbiddenai.gamefinder.domain.Game;
 import me.theforbiddenai.gamefinder.exception.GameRetrievalException;
+import me.theforbiddenai.gamefinder.exception.WebScrapeException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class GOGWebScrape extends WebScraper {
+public class GOGWebScrape extends WebScraper<JsonNode> {
 
-    // Maps a json field name to it's corresponding group name in the PRODUCT_CARD_REGEX regex
-    private static final Map<String, String> PRODUCT_CARD_FIELD_MAP = Map.ofEntries(
-            Map.entry("cardProduct", "prodVal"),
-            Map.entry("currency", "curVal"),
-            Map.entry("cardProductPromoEndDate", "promoVal")
-    );
-
-    private static final Pattern PRODUCT_CARD_REGEX = Pattern.compile(
-            "(?<prod>cardProduct):\\s(?<prodVal>\\{.*})|" +
-                    "(?<cur>currency):\\s(?<curVal>\".*\")|" +
-                    "window\\.productcardData\\.(?<promo>cardProductPromoEndDate)\\s=\\s(?<promoVal>\\{.*})"
-            , Pattern.MULTILINE);
+    private static final String JSON_FIELD_FORMAT = "\"%s\":%s";
+    private static final int PRODUCT_CARD_FIELD_COUNT = 3;
 
     public GOGWebScrape(ObjectMapper mapper) {
         super(mapper, "gog_wantsmaturecontent=9999");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    protected void modifyGameAttributes(String html, Game game) throws GameRetrievalException, IOException {
-        JsonNode productCard = getProductCardData(html, game.getUrl());
+    protected void modifyGameAttributes(JsonNode jsonNode, Game game) throws GameRetrievalException, IOException {
+        System.out.println(jsonNode);
     }
 
     /**
-     * Gets the productCard data for a GOG game
-     *
-     * @param html The HTML for a GOG game page
-     * @param url  The URL to the GOG game page
-     * @return A JsonNode containing all the productCard data
-     * @throws GameRetrievalException  If the necessary data is not found
-     * @throws JsonProcessingException If the object mapper is unable to parse the retrieved json
+     * {@inheritDoc}
      */
-    protected JsonNode getProductCardData(@NonNull String html, String url) throws GameRetrievalException, IOException {
-
-        // This reason I am using a StringBuilder over an ObjectNode is because I only need to call readTree once,
-        // whereas with an ObjectNode, readTree would need to be called for almost every fieldValue
+    @Override
+    protected JsonNode processHTML(InputStream inputStream, String url) throws WebScrapeException {
         StringBuilder jsonBuilder = new StringBuilder("{");
-
-        // This set keeps track of all retrieved productCard fields
         Set<String> foundFields = new HashSet<>();
 
-        int productCardMapSize = PRODUCT_CARD_FIELD_MAP.size();
-        Matcher cardProductMatcher = PRODUCT_CARD_REGEX.matcher(html);
+        Scanner scanner = new Scanner(inputStream);
 
-        while (cardProductMatcher.find()) {
-            // All required fields have been retrieved, break the loop
-            // This is necessary because sometimes these values are repeated
-            if (foundFields.size() == productCardMapSize) break;
+        // Loop through the HTML lines
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
 
-            // Pull out the fieldName from the first nonnull group: prod, cur, or promo
-            String fieldName = Optional.ofNullable(cardProductMatcher.group("prod"))
-                    .or(() -> Optional.ofNullable(cardProductMatcher.group("cur")))
-                    .or(() -> Optional.ofNullable(cardProductMatcher.group("promo")))
-                    .orElse(null);
+            String jsonField = getJsonField(line, foundFields);
+            if (jsonField == null) continue;
 
-            // Make sure that the fieldName isn't null and hasn't already been found
-            if (fieldName == null || foundFields.contains(fieldName)) continue;
+            jsonBuilder.append(jsonField).append(",");
 
-            String valueGroupName = PRODUCT_CARD_FIELD_MAP.get(fieldName);
-
-            // Pull out the fieldValue for the found fieldName
-            String fieldValue = cardProductMatcher.group(valueGroupName);
-
-            // Make sure the field value isn't null
-            if (fieldValue == null) continue;
-
-            foundFields.add(fieldName);
-
-            // Add the fieldName to jsonBuilder in proper json syntax
-            jsonBuilder.append("\"").append(fieldName).append("\":");
-
-            // Add fieldValue to jsonBuilder
-            jsonBuilder.append(fieldValue).append(",");
+            // All required fields have been found
+            if (foundFields.size() == PRODUCT_CARD_FIELD_COUNT) break;
         }
 
-        // Make sure all data has been retrieved
-        if (foundFields.size() != productCardMapSize) {
-            throw new GameRetrievalException("Unable to retrieve required data for GOG game with URL " + url);
+        if (foundFields.size() != PRODUCT_CARD_FIELD_COUNT) {
+            throw new WebScrapeException("Unable to retrieve all required product card data for GOG game with url " + url);
         }
 
-        // Remove hanging comma
+        // Remove hanging comma and add closing curly brace
         jsonBuilder.deleteCharAt(jsonBuilder.length() - 1);
-
-        // Close Json object
         jsonBuilder.append("}");
 
-        return super.getMapper().readTree(jsonBuilder.toString());
+        scanner.close();
+
+        try {
+            return getMapper().readTree(jsonBuilder.toString());
+        } catch (JsonProcessingException ex) {
+            throw new WebScrapeException("Unable to parse json data for GOG game with url " + url, ex);
+        }
     }
+
+
+    /**
+     * Extracts a required product card json fields (cardProduct, currency, and cardProductPromoEndDate) from a line
+     * if it is there
+     *
+     * @param line        The line the json field is being extracted from
+     * @param foundFields A set containing the name of all found product card fields
+     * @return A json string containing the field, or null if it cannot be found in the given line or if it has already been found
+     */
+    private String getJsonField(String line, Set<String> foundFields) {
+        String name = "";
+        String value = "";
+
+        if (line.startsWith("cardProduct:")) {
+            name = "cardProduct";
+            value = getJsonValue(line, "cardProduct:", "}");
+        }
+
+        if (line.startsWith("currency:")) {
+            name = "currency";
+            value = getJsonValue(line, "currency:", "\"");
+        }
+
+        if (line.startsWith("window.productcardData.cardProductPromoEndDate =")) {
+            name = "cardProductPromoEndDate";
+            value = getJsonValue(line, "window.productcardData.cardProductPromoEndDate =", "}");
+        }
+
+        // Make sure that there is a valid name and value (name can only ever be empty not blank)
+        if (name.isEmpty() || value.isBlank()) return null;
+
+        // Return null if this field has already been retrieved
+        if (foundFields.contains(name)) return null;
+
+        foundFields.add(name);
+        return String.format(JSON_FIELD_FORMAT, name, value);
+    }
+
+    /**
+     * Extracts a json field value from a given line
+     *
+     * @param line         The line the value is being extracted from
+     * @param removePrefix The prefix being removed from the line
+     * @param endWith      The character that the string must end with
+     * @return The found value or an empty string if not found
+     */
+    private String getJsonValue(String line, String removePrefix, String endWith) {
+        try {
+            // Removes the prefix
+            String value = line.substring(removePrefix.length());
+            if (value.endsWith(endWith)) return value;
+
+            // Makes sure the value ends at the last instance of endWith
+            return value.substring(0, value.lastIndexOf(endWith) + 1);
+        } catch (IndexOutOfBoundsException ex) {
+            return "";
+        }
+    }
+
 }
--- src/main/java/me/theforbiddenai/gamefinder/webscraper/WebScraper.java
+++ src/main/java/me/theforbiddenai/gamefinder/webscraper/WebScraper.java
@@ -13,18 +13,28 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 
-public abstract class WebScraper {
+/**
+ * Responsible for web scraping data from a game page
+ *
+ * @param <T> The type of object that will be used to store any retrieved data from the HTML of a game page
+ * @author TheForbiddenAi
+ */
+public abstract class WebScraper<T> {
 
     protected static final GameFinderConfiguration CONFIG = GameFinderConfiguration.getInstance();
     private static final int TIMEOUT_SECONDS = 10;
-    @Getter(AccessLevel.PROTECTED)
-    private final ObjectMapper mapper;
+
     private final String cookies;
     private final OkHttpClient httpClient;
+
+    @Getter(AccessLevel.PROTECTED)
+    private final ObjectMapper mapper;
+
 
     public WebScraper(ObjectMapper mapper, String cookies) {
         this.mapper = mapper;
@@ -40,7 +50,7 @@ public abstract class WebScraper {
      * @return A CompletableFuture containing the updated game
      */
     public CompletableFuture<Game> modifyGameAttributes(Game game) {
-        return CompletableFuture.supplyAsync(() -> getHTML(game.getUrl()), CONFIG.getExecutorService())
+        return CompletableFuture.supplyAsync(() -> getHTMLData(game.getUrl()), CONFIG.getExecutorService())
                 .thenApply(html -> {
                     try {
                         modifyGameAttributes(html, game);
@@ -54,21 +64,32 @@ public abstract class WebScraper {
     /**
      * Adds the remaining information to a game object. What data needs to be added is dependent on the game's platform
      *
-     * @param html The html of the game's listing page
+     * @param t    The data object containing the data required to complete a game object
      * @param game The game object being updated
      * @throws GameRetrievalException If there is some error updating the game
      * @throws IOException            If there is some issue parsing the html
      */
-    protected abstract void modifyGameAttributes(String html, Game game) throws GameRetrievalException, IOException;
+    protected abstract void modifyGameAttributes(T t, Game game) throws GameRetrievalException, IOException;
 
     /**
-     * Gets a website's HTML
+     * Retrieves the data needed to complete a game object from the HTML of a game page
+     *
+     * @param inputStream The HTML of the page
+     * @param url         The url of the game page
+     * @return An object containing the data required to complete a game object
+     * @throws WebScrapeException If the required data is unable to be retrieved
+     */
+    protected abstract T processHTML(InputStream inputStream, String url) throws WebScrapeException;
+
+
+    /**
+     * Gets the required data from a website's HTML
      *
      * @param url The url of the website
-     * @return A string containing the page's HTML
+     * @return A data object containing the information specified in {@link #processHTML(InputStream, String)}
      * @throws WebScrapeException If the request to connect to the website fails or the response body is null
      */
-    private String getHTML(String url) throws WebScrapeException {
+    private T getHTMLData(String url) throws WebScrapeException {
         Request request = new Request.Builder()
                 .url(url)
                 .header("cookie", this.cookies)
@@ -80,7 +101,8 @@ public abstract class WebScraper {
             if (responseBody == null)
                 throw new WebScrapeException("Unable to retrieve HTML from " + url);
 
-            return responseBody.string();
+
+            return processHTML(responseBody.byteStream(), url);
         } catch (IOException ex) {
             throw new WebScrapeException("Unable to connect to " + url, ex);
         }
